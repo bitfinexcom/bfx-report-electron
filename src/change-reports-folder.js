6f713bf6016291d2eef1fedb7cc173b90907cde2
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-07-14 18:20:46 +0300

    Change report folder changing to support new electron api

--- src/change-reports-folder.js
+++ src/change-reports-folder.js
@@ -14,52 +14,59 @@ const { getConfigsKeeperByName } = require('./configs-keeper')
 module.exports = ({ pathToUserDocuments }) => {
   const dialog = electron.dialog || electron.remote.dialog
 
-  return () => {
+  return async () => {
     const win = electron.BrowserWindow.getFocusedWindow()
 
-    dialog.showOpenDialog(
-      win,
-      {
-        title: 'Change reports folder',
-        defaultPath: pathToUserDocuments,
-        buttonLabel: 'Select',
-        properties: [
-          'openDirectory',
-          'createDirectory',
-          'promptToCreate',
-          'treatPackageAsDirectory'
-        ]
-      },
-      async (files) => {
-        try {
-          if (
-            !Array.isArray(files) ||
-            files.length === 0
-          ) {
-            return
-          }
-          if (files.some(file => (
-            !file || typeof file !== 'string'
-          ))) {
-            throw new InvalidFilePathError()
-          }
-
-          await pauseApp()
-          const isSaved = await getConfigsKeeperByName('main')
-            .saveConfigs({ pathToUserCsv: files[0] })
-
-          if (!isSaved) {
-            throw new ReportsFolderChangingError()
-          }
-
-          relaunch()
-        } catch (err) {
-          await showErrorModalDialog(win, 'Change reports folder', err)
-
-          console.error(err)
-          relaunch()
+    try {
+      const {
+        filePaths,
+        canceled
+      } = await dialog.showOpenDialog(
+        win,
+        {
+          title: 'Change reports folder',
+          defaultPath: pathToUserDocuments,
+          buttonLabel: 'Select',
+          properties: [
+            'openDirectory',
+            'createDirectory',
+            'promptToCreate',
+            'treatPackageAsDirectory'
+          ]
         }
+      )
+
+      if (
+        canceled ||
+        !Array.isArray(filePaths) ||
+        filePaths.length === 0
+      ) {
+        return
       }
-    )
+      if (filePaths.some(file => (
+        !file || typeof file !== 'string'
+      ))) {
+        throw new InvalidFilePathError()
+      }
+
+      await pauseApp()
+      const isSaved = await getConfigsKeeperByName('main')
+        .saveConfigs({ pathToUserCsv: filePaths[0] })
+
+      if (!isSaved) {
+        throw new ReportsFolderChangingError()
+      }
+
+      relaunch()
+    } catch (err) {
+      try {
+        await showErrorModalDialog(win, 'Change reports folder', err)
+      } catch (err) {
+        console.error(err)
+      }
+
+      console.error(err)
+      relaunch()
+    }
   }
 }
