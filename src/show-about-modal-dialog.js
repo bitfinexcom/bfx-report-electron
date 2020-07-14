8e8d122cb00e55e7f9100afb7875182535a7a6c2
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-07-14 16:17:55 +0300

    Change about modal dialog to support new electron api

--- src/show-about-modal-dialog.js
+++ src/show-about-modal-dialog.js
@@ -23,53 +23,54 @@ try {
 module.exports = () => {
   const _dialog = dialog || remote.dialog
 
-  return () => {
-    const win = BrowserWindow.getFocusedWindow()
-    const productName = 'Bitfinex Report'
+  return async () => {
+    try {
+      const win = BrowserWindow.getFocusedWindow()
+      const productName = 'Bitfinex Report'
 
-    const type = os.type()
-    const arch = os.arch()
-    const release = os.release()
+      const type = os.type()
+      const arch = os.arch()
+      const release = os.release()
 
-    const detail = `
-Version: ${packageJson.version}${EOL}\
-Commit: ${lastCommit.hash}${EOL}\
-Date:  ${lastCommit.date}${EOL}\
-Electron: ${process.versions.electron}${EOL}\
-Chrome: ${process.versions.chrome}${EOL}\
-Node.js: ${process.versions.node}${EOL}\
-V8: ${process.versions.v8}${EOL}\
-OS: ${type} ${arch} ${release}\
-`
+      const detail = `
+  Version: ${packageJson.version}${EOL}\
+  Commit: ${lastCommit.hash}${EOL}\
+  Date:  ${lastCommit.date}${EOL}\
+  Electron: ${process.versions.electron}${EOL}\
+  Chrome: ${process.versions.chrome}${EOL}\
+  Node.js: ${process.versions.node}${EOL}\
+  V8: ${process.versions.v8}${EOL}\
+  OS: ${type} ${arch} ${release}\
+  `
 
-    _dialog.showMessageBox(
-      win,
-      {
-        type: 'info',
-        title: productName,
-        message: productName,
-        detail,
-        buttons: ['Copy', 'GitHub', 'OK'],
-        defaultId: 2,
-        cancelId: 2,
-        icon: path.join(__dirname, '../build/icons/64x64.png')
-      },
-      async (btnId) => {
-        try {
-          if (btnId === 2) {
-            return
-          }
-          if (btnId === 1) {
-            shell.openExternal(
-              packageJson.repository || 'https://github.com'
-            )
-          }
-
-          clipboard.writeText(detail)
-        } catch (err) {
-          console.error(err)
+      const {
+        response: btnId
+      } = await _dialog.showMessageBox(
+        win,
+        {
+          type: 'info',
+          title: productName,
+          message: productName,
+          detail,
+          buttons: ['Copy', 'GitHub', 'OK'],
+          defaultId: 2,
+          cancelId: 2,
+          icon: path.join(__dirname, '../build/icons/64x64.png')
         }
+      )
+
+      if (btnId === 2) {
+        return
       }
-    )
+      if (btnId === 1) {
+        shell.openExternal(
+          packageJson.repository || 'https://github.com'
+        )
+      }
+
+      clipboard.writeText(detail)
+    } catch (err) {
+      console.error(err)
+    }
   }
 }
