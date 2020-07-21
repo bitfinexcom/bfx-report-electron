3ffa2c7c2ecf26047eb61f2e82a8edb65008a027
Vladimir Voronkov <vsvoronkov@gmail.com>
2020-07-21 18:39:47 +0300

    Add prebuild ed25519-supercop binaries

--- scripts/reinstall-deps.sh
+++ scripts/reinstall-deps.sh
@@ -123,7 +123,18 @@ function postInstall {
 }
 
 npmInstall $ROOT "--isDevNeeded"
+ed25519SupercopBinBasePath="$targetPlatform-$ARCH/electron.abi80.node"
+ed25519SupercopBinVendorPath="$ROOT/build/vendors/ed25519-supercop/$ed25519SupercopBinBasePath"
+ed25519SupercopBinPrebuildPath="$ROOT/node_modules/ed25519-supercop/prebuilds/$ed25519SupercopBinBasePath"
+ed25519SupercopBinPath="$ROOT/node_modules/ed25519-supercop/build/Release/supercop.node"
+
+if ! [ -f "$ed25519SupercopBinVendorPath" ]; then
+  exit 1
+fi
+
+cp -f $ed25519SupercopBinVendorPath $ed25519SupercopBinPrebuildPath
 postInstall $ROOT "prod"
+cp -f $ed25519SupercopBinVendorPath $ed25519SupercopBinPath
 
 npmInstall $expressFolder
 postInstall $expressFolder
