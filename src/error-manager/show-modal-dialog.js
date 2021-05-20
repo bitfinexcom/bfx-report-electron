5b9b53fe744af7c7f1b2ad0600349979f2cbc7af
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-05-20 18:09:50 +0300

    Show error box for unhandled errors on mac and win

--- src/error-manager/show-modal-dialog.js
+++ src/error-manager/show-modal-dialog.js
@@ -1,9 +1,43 @@
 'use strict'
 
+const { app, dialog } = require('electron')
+
 // TODO:
-module.exports = async (mdIssue) => {
-  return {
-    isNewGithubIssueOpened: true,
-    isCanceled: false
+module.exports = async (params) => {
+  const {
+    errBoxTitle = 'Bug report',
+    errBoxDescription = 'A new Github issue will be opened',
+    mdIssue
+  } = params
+
+  // TODO: Needs to add BrowserWindow to render markdown converted to html
+  if (app.isReady()) {
+    return {
+      isExit: false,
+      isReported: true,
+      isIgnored: false
+    }
   }
+
+  const res = {
+    isExit: true,
+    isReported: true,
+    isIgnored: false
+  }
+
+  // If called before the app ready event on Linux,
+  // the message will be emitted to stderr,
+  // and no GUI dialog will appear
+  if (process.platform !== 'linux') {
+    dialog.showErrorBox(
+      errBoxTitle,
+      errBoxDescription
+    )
+
+    return res
+  }
+
+  // TODO: On Linux needs to spawn zenity gui tool to show error
+
+  return res
 }
