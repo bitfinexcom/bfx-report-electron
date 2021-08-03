24266c93ef6d6d21faec905e0662dc3550a9f496
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-08-03 16:20:15 +0300

    Fix appimage build relaunching

--- src/relaunch.js
+++ src/relaunch.js
@@ -5,6 +5,15 @@ const electron = require('electron')
 module.exports = () => {
   const app = electron.app || electron.remote.app
 
-  app.relaunch({ args: process.argv.slice(1).concat(['--relaunch']) })
+  const options = {
+    args: process.argv.slice(1).concat(['--relaunch'])
+  }
+
+  if (process.env.APPIMAGE) {
+    options.execPath = process.env.APPIMAGE
+    options.args.unshift('--appimage-extract-and-run')
+  }
+
+  app.relaunch(options)
   app.exit(0)
 }
