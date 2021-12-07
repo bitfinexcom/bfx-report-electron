58b664a7964f5cc6243385010cdc13cf410b6244
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-12-07 17:48:52 +0200

    Add ability to show changelog after app is updated

--- src/changelog-manager/manage-changelog.js
+++ src/changelog-manager/manage-changelog.js
@@ -1,7 +1,27 @@
 'use strict'
 
+const { compare } = require('compare-versions')
+
+const {
+  getConfigsKeeperByName
+} = require('../configs-keeper')
+const getDebugInfo = require('../helpers/get-debug-info')
+
 const showChangelog = require('./show-changelog')
 
-module.exports = () => {
-  showChangelog()
+module.exports = async () => {
+  try {
+    const { version } = getDebugInfo()
+    const configsKeeper = getConfigsKeeperByName('main')
+    const shownChangelogVer = await configsKeeper
+      .getConfigByName('shownChangelogVer')
+
+    if (compare(version, shownChangelogVer, '<=')) {
+      return
+    }
+
+    await showChangelog()
+  } catch (err) {
+    console.error(err)
+  }
 }
