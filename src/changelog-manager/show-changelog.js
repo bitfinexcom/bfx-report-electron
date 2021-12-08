220ce48ff210a84ba029b80db12b99fd890e05c5
Vladimir Voronkov <vsvoronkov@gmail.com>
2021-12-08 16:25:23 +0200

    Parse changelog

--- src/changelog-manager/show-changelog.js
+++ src/changelog-manager/show-changelog.js
@@ -1,8 +1,19 @@
 'use strict'
 
+const path = require('path')
+const parseChangelog = require('changelog-parser')
+const { rootPath } = require('electron-root-path')
+
+const changelogPath = path.join(rootPath, 'CHANGELOG.md')
+
 // TODO:
 module.exports = async () => {
   try {
+    const mdEntries = await parseChangelog({
+      filePath: changelogPath,
+      removeMarkdown: false
+    })
+
     return true
   } catch (err) {
     console.error(err)
