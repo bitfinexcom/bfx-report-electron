a923c5bc3ea6596d03959f9bbf79b612f09de5e2
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-05-04 15:13:54 +0300

    Add bash helper fn to get conf value from package json

---
+++ scripts/helpers/get-conf-value.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function getConfValue {
+  local COLOR_RED="${COLOR_RED:-"\033[31m"}"
+  local COLOR_NORMAL="${COLOR_NORMAL:-"\033[39m"}"
+
+  if [ -z "${1:-}" ]; then
+    echo -e "\n${COLOR_RED}Requires the first argument of '${FUNCNAME[0]}' fn as dependency name!${COLOR_NORMAL}" >&2
+    exit 1
+  fi
+  if [ -z "${2:-}" ]; then
+    echo -e "\n${COLOR_RED}Requires the first argument of '${FUNCNAME[0]}' fn as path to folder where package.json!${COLOR_NORMAL}" >&2
+    exit 1
+  fi
+
+  local dep="$1"
+  local path="$2"
+
+  local version=$(cat "$path/package.json" \
+    | grep \"$dep\" \
+    | head -1 \
+    | awk -F: '{ print $2($3 ? ":" : "")$3($4 ? ":" : "")$4 }' \
+    | sed 's/[",]//g' \
+    | sed 's/#.*$//' \
+    | tr -d '[[:space:]]')
+
+  echo "$version"
+}
