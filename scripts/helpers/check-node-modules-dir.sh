23232071a992f9d5103715a1ef1e3dce5467fe0e
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-05-13 07:32:29 +0300

    Fix path for checking node_module folder existence

--- scripts/helpers/check-node-modules-dir.sh
+++ scripts/helpers/check-node-modules-dir.sh
@@ -4,14 +4,16 @@ function checkNodeModulesDir {
   local COLOR_RED=${COLOR_RED:-"\033[31m"}
   local COLOR_NORMAL=${COLOR_NORMAL:-"\033[39m"}
 
-  if [ -z "${1:-"."}" ]; then
+  if [ -z "${1:-}" ]; then
     echo -e "\n${COLOR_RED}Requires the first argument of '${FUNCNAME[0]}' fn as path!${COLOR_NORMAL}" >&2
     exit 1
   fi
 
-  if ! [ -d "$1/node_modules" ]; then
+  local path="${1:-"."}"
+
+  if ! [ -d "$path/node_modules" ]; then
     echo -e "\n${COLOR_RED}NPM dependencies have not been installed \
-in the '$1' root dir!${COLOR_NORMAL}" >&2
+in the '$path' root dir!${COLOR_NORMAL}" >&2
     exit 1
   fi
 }
