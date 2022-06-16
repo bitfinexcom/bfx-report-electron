58c6d901670f1fc807ceb07dc94a84558bfb8e37
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-06-16 15:21:55 +0300

    Fix changing dir ownership

--- scripts/helpers/change-dir-ownership-to-curr-user.sh
+++ scripts/helpers/change-dir-ownership-to-curr-user.sh
@@ -3,7 +3,7 @@
 function changeDirOwnershipToCurrUser {
   local path="${1:-}"
   local defaultUID="${CURRENT_UID:-"1000:1000"}"
-  local currUID="${2:-defaultUID}"
+  local currUID="${2:-"$defaultUID"}"
 
   if [ -n "$path" ] && [ -d "$path" ]; then
     chown -R "$currUID" "$path" || [[ $? == 1 ]]
