97955708bb6a80115d5f3a460a416c548559643d
Vladimir Voronkov <vsvoronkov@gmail.com>
2022-06-16 15:03:05 +0300

    Add helper to change dir ownership to curr user

---
+++ scripts/helpers/change-dir-ownership-to-curr-user.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+function changeDirOwnershipToCurrUser {
+  local path="${1:-}"
+  local defaultUID="${CURRENT_UID:-"1000:1000"}"
+  local currUID="${2:-defaultUID}"
+
+  if [ -n "$path" ] && [ -d "$path" ]; then
+    chown -R "$currUID" "$path" || [[ $? == 1 ]]
+  fi
+}
