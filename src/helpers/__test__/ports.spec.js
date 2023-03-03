1367509a79dfd2cffef492968e7ffac1f03ec463
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-03-03 13:31:55 +0200

    Add lookup free ports test cases

---
+++ src/helpers/__test__/ports.spec.js
@@ -0,0 +1,119 @@
+'use strict'
+
+const { assert } = require('chai')
+const { Server } = require('net')
+const DHT = require('bittorrent-dht')
+
+const {
+  getFreePort,
+  isDHTPort,
+  getMaxPort,
+  getDefaultPorts,
+  getPortRangesForLookingUp
+} = require('../ports')
+
+const checkAssertions = (res) => {
+  assert.isObject(res)
+  assert.containsAllKeys(res, [
+    'grape1DhtPort',
+    'grape1ApiPort',
+    'grape2DhtPort',
+    'grape2ApiPort',
+    'workerApiPort',
+    'workerWsPort',
+    'expressApiPort'
+  ])
+
+  const maxPort = getMaxPort()
+  const defaultPorts = getDefaultPorts()
+  const ranges = getPortRangesForLookingUp()
+
+  for (const [name, port] of Object.entries(res)) {
+    assert.isAtLeast(port, defaultPorts[name])
+    assert.isAtMost(port, maxPort)
+
+    const errors = []
+
+    for (const { from, to } of ranges) {
+      try {
+        assert.isAtLeast(port, from)
+        assert.isAtMost(port, to)
+      } catch (err) {
+        errors.push(err)
+      }
+    }
+
+    if (errors.length >= ranges.length) {
+      throw errors[0]
+    }
+  }
+}
+
+describe('getFreePort helper', () => {
+  it('it should lookup free ports', async function () {
+    const res = await getFreePort()
+
+    checkAssertions(res)
+  })
+
+  it('it should lookup free ports while the previous 3 lookups have been used', async function () {
+    const dhts = []
+    const srvs = []
+
+    for (let i = 0; i < 3; i += 1) {
+      const newPorts = await getFreePort()
+
+      for (const [newName, newPort] of Object.entries(newPorts)) {
+        if (isDHTPort(newName)) {
+          const dht = new DHT()
+          dhts.push(dht)
+
+          await new Promise((resolve, reject) => {
+            dht.once('error', (err) => {
+              dht.removeListener('listening', resolve)
+              reject(err)
+            })
+            dht.once('listening', () => {
+              dht.removeListener('error', reject)
+              resolve()
+            })
+            dht.listen(newPort)
+          })
+
+          continue
+        }
+
+        const srv = new Server()
+        srv.unref()
+        srvs.push(srv)
+
+        await new Promise((resolve, reject) => {
+          srv.once('error', (err) => {
+            srv.removeListener('listening', resolve)
+            reject(err)
+          })
+          srv.once('listening', () => {
+            srv.removeListener('error', reject)
+            resolve()
+          })
+          srv.listen(newPort)
+        })
+      }
+    }
+
+    const res = await getFreePort()
+
+    checkAssertions(res)
+
+    const promises = []
+
+    for (const dht of dhts) {
+      promises.push(new Promise((resolve) => dht.destroy(resolve)))
+    }
+    for (const srv of srvs) {
+      promises.push(new Promise((resolve) => srv.close(resolve)))
+    }
+
+    await Promise.all(promises)
+  })
+})
