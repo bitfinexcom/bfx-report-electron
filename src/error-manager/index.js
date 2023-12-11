9ac18f5375d460ce0e578dd96bb5860213298d4c
Vladimir Voronkov <vsvoronkov@gmail.com>
2023-12-11 10:02:09 +0200

    Add exception for network timeout error

--- src/error-manager/index.js
+++ src/error-manager/index.js
@@ -84,7 +84,8 @@ const _isLogSkipped = (log) => {
       // Skip error when can't get code signature on mac
       str.includes('Could not get code signature') ||
       str.includes('ERR_BFX_API_SERVER_IS_NOT_AVAILABLE') ||
-      str.includes('database is locked')
+      str.includes('database is locked') ||
+      str.includes('network timeout')
     )
   )
 }
