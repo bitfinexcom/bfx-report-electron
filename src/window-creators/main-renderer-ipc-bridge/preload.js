87ae4fe927587df0db42c833a72b73dffac43eca
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-09-25 14:54:50 +0300

    Add translation ipc channel bridge to preload

---
+++ src/window-creators/main-renderer-ipc-bridge/preload.js
@@ -0,0 +1,36 @@
+'use strict'
+
+const { contextBridge, ipcRenderer } = require('electron')
+const isTestEnv = process.env.NODE_ENV === 'test'
+
+const CHANNEL_NAMES = {
+  TRANSLATIONS: 'translations'
+}
+
+const INVOKE_METHOD_NAMES = {
+  GET_DEFAULT_LANG: 'getDefaultLang'
+}
+
+const getEventName = (channel, method) => {
+  return `${channel}:${method}`
+}
+
+const invoke = (channel, method, args) => {
+  const eventName = getEventName(channel, method)
+
+  return ipcRenderer.invoke(eventName, args)
+}
+
+const bfxReportElectronApi = {}
+
+for (const methodName of Object.values(INVOKE_METHOD_NAMES)) {
+  bfxReportElectronApi[methodName] = (args) => {
+    return invoke(CHANNEL_NAMES.TRANSLATIONS, methodName, args)
+  }
+}
+
+if (isTestEnv) {
+  require('wdio-electron-service/preload')
+}
+
+contextBridge.exposeInMainWorld('bfxReportElectronApi', bfxReportElectronApi)
