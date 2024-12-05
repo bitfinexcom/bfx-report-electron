dd1bfcb6aba3abdf4ffe749b10b76929d0191312
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-12-05 11:22:30 +0200

    Add ability to serialize menu for ipc

--- src/window-creators/main-renderer-ipc-bridge/menu-ipc-channel-handlers.js
+++ src/window-creators/main-renderer-ipc-bridge/menu-ipc-channel-handlers.js
@@ -1,29 +1,82 @@
 'use strict'
 
+const { Menu } = require('electron')
+
 const IpcChannelHandlers = require('./ipc.channel.handlers')
-const createMenu = require('../../create-menu')
 
 class MenuIpcChannelHandlers extends IpcChannelHandlers {
   static channelName = 'menu'
 
-  #menuTemplateFactory () {
-    if (this.menuTemplate) {
-      return this.menuTemplate
+  #serializeMenu (menu) {
+    if (
+      !Array.isArray(menu?.items) ||
+      menu.items.length === 0
+    ) {
+      return []
     }
 
-    this.menuTemplate = createMenu({
-      shouldMenuTemplateBeReturned: true
-    })
+    const serializedMenu = []
 
-    return this.menuTemplate
+    for (const [i, menuItem] of menu.items.entries()) {
+      const {
+        label,
+        id,
+        type,
+        accelerator,
+        enabled,
+        visible,
+        checked,
+        submenu
+      } = menuItem ?? {}
+
+      const serializedMenuItem = {
+        label,
+        id,
+        type,
+        accelerator,
+        enabled,
+        visible,
+        checked: typeof checked === 'function'
+          ? checked()
+          : checked,
+        submenu: this.#serializeMenu(submenu)
+      }
+      const hasSubmenu = (
+        Array.isArray(serializedMenuItem.submenu) &&
+        serializedMenuItem.submenu.length > 0
+      )
+      const isSeparator = (
+        type === 'separator' &&
+        serializedMenu[i - 1]?.type &&
+        serializedMenu[i - 1]?.type !== 'separator'
+      )
+
+      if (!hasSubmenu) {
+        serializedMenuItem.submenu = null
+      }
+      if (isSeparator) {
+        serializedMenu.push(serializedMenuItem)
+
+        continue
+      }
+      if (
+        !id &&
+        !hasSubmenu
+      ) {
+        continue
+      }
+
+      serializedMenu.push(serializedMenuItem)
+    }
+
+    return serializedMenu
   }
 
   async getMenuTemplateHandler (event, args) {
-    const menuTemplate = createMenu({
-      shouldMenuTemplateBeReturned: true
-    })
+    const menu = Menu.getApplicationMenu()
+    const serializedMenu = this.#serializeMenu(menu)
 
-    return menuTemplate
+    return serializedMenu
   }
 }
 
