8928d1fd3f1acf03df098e207eed7f3f8e6b449a
Vladimir Voronkov <vsvoronkov@gmail.com>
2024-12-06 14:19:30 +0200

    Customize menu item template to be serialized for ui

--- src/create-menu/index.js
+++ src/create-menu/index.js
@@ -79,6 +79,7 @@ module.exports = (params) => {
   const menuTemplate = [
     ...(isMac
       ? [{
+          role: 'appMenu',
           label: app.name,
           submenu: [
             {
@@ -86,41 +87,144 @@ module.exports = (params) => {
                 'menu.helpSubMenu.aboutLabel',
                 { appName: app.name }
               ),
+              id: MENU_ITEM_IDS.MAC_ABOUT_MENU_ITEM,
               click: showAboutModalDialog()
             },
             { type: 'separator' },
             {
               role: 'services',
-              label: i18next.t('menu.macMainSubmenu.servicesLabel')
+              label: i18next.t('menu.macMainSubmenu.servicesLabel'),
+              id: MENU_ITEM_IDS.MAC_SERVICES_MENU_ITEM
             },
             { type: 'separator' },
             {
               role: 'hide',
-              label: i18next.t('menu.macMainSubmenu.hideLabel')
+              label: i18next.t('menu.macMainSubmenu.hideLabel'),
+              id: MENU_ITEM_IDS.MAC_HIDE_MENU_ITEM
             },
             {
               role: 'hideOthers',
-              label: i18next.t('menu.macMainSubmenu.hideOthersLabel')
+              label: i18next.t('menu.macMainSubmenu.hideOthersLabel'),
+              id: MENU_ITEM_IDS.MAC_HIDE_OTHERS_MENU_ITEM
             },
             {
               role: 'unhide',
-              label: i18next.t('menu.macMainSubmenu.unhideLabel')
+              label: i18next.t('menu.macMainSubmenu.unhideLabel'),
+              id: MENU_ITEM_IDS.MAC_UNHIDE_MENU_ITEM
             },
             { type: 'separator' },
             {
               role: 'quit',
-              label: i18next.t('menu.macMainSubmenu.quitLabel')
+              label: i18next.t('menu.macMainSubmenu.quitLabel'),
+              id: MENU_ITEM_IDS.MAC_QUIT_MENU_ITEM
             }
           ]
         }]
       : []),
-    { role: 'fileMenu', label: i18next.t('menu.fileSubMenu.label') },
-    { role: 'editMenu', label: i18next.t('menu.editSubMenu.label') },
     {
+      role: 'fileMenu',
+      label: i18next.t('menu.fileSubMenu.label'),
+      submenu: [
+        isMac
+          ? {
+              role: 'close',
+              label: 'Close', // TODO:
+              id: MENU_ITEM_IDS.MAC_CLOSE_MENU_ITEM
+            }
+          : {
+              role: 'quit',
+              label: 'Quit', // TODO:
+              id: MENU_ITEM_IDS.QUIT_MENU_ITEM
+            }
+      ]
+    },
+    {
+      role: 'editMenu',
+      label: i18next.t('menu.editSubMenu.label'),
+      submenu: [
+        {
+          role: 'undo',
+          label: 'Undo', // TODO:
+          id: MENU_ITEM_IDS.UNDO_MENU_ITEM
+        },
+        {
+          role: 'redo',
+          label: 'Redo', // TODO:
+          id: MENU_ITEM_IDS.REDO_MENU_ITEM
+        },
+        { type: 'separator' },
+        {
+          role: 'cut',
+          label: 'Cut', // TODO:
+          id: MENU_ITEM_IDS.CUT_MENU_ITEM
+        },
+        {
+          role: 'copy',
+          label: 'Copy', // TODO:
+          id: MENU_ITEM_IDS.COPY_MENU_ITEM
+        },
+        {
+          role: 'paste',
+          label: 'Paste', // TODO:
+          id: MENU_ITEM_IDS.PASTE_MENU_ITEM
+        },
+        ...(isMac
+          ? [
+              {
+                role: 'pasteAndMatchStyle',
+                label: 'Paste And Match Style', // TODO:
+                id: MENU_ITEM_IDS.MAC_PASTE_AND_MATCH_STYLE_MENU_ITEM
+              },
+              {
+                role: 'delete',
+                label: 'Delete', // TODO:
+                id: MENU_ITEM_IDS.DELETE_MENU_ITEM
+              },
+              {
+                role: 'selectAll',
+                label: 'Select All', // TODO:
+                id: MENU_ITEM_IDS.SELECT_ALL_MENU_ITEM
+              },
+              { type: 'separator' },
+              {
+                label: 'Speech',
+                submenu: [
+                  {
+                    role: 'startSpeaking',
+                    label: 'Start Speaking', // TODO:
+                    id: MENU_ITEM_IDS.MAC_START_SPEAKING_MENU_ITEM
+                  },
+                  {
+                    role: 'stopSpeaking',
+                    label: 'Stop Speaking', // TODO:
+                    id: MENU_ITEM_IDS.MAC_STOP_SPEAKING_MENU_ITEM
+                  }
+                ]
+              }
+            ]
+          : [
+              {
+                role: 'delete',
+                label: 'Delete', // TODO:
+                id: MENU_ITEM_IDS.DELETE_MENU_ITEM
+              },
+              { type: 'separator' },
+              {
+                role: 'selectAll',
+                label: 'Select All', // TODO:
+                id: MENU_ITEM_IDS.SELECT_ALL_MENU_ITEM
+              }
+            ])
+      ]
+    },
+    {
+      role: 'viewMenu',
       label: i18next.t('menu.viewSubMenu.label'),
       submenu: [
         {
+          role: 'reload',
           label: i18next.t('menu.viewSubMenu.reloadLabel'),
+          id: MENU_ITEM_IDS.RELOAD_MENU_ITEM,
           accelerator: 'CmdOrCtrl+R',
           click: (item, focusedWindow) => {
             if (focusedWindow) {
@@ -131,7 +235,9 @@ module.exports = (params) => {
           }
         },
         {
+          role: 'forceReload',
           label: i18next.t('menu.viewSubMenu.forceReloadLabel'),
+          id: MENU_ITEM_IDS.FORCE_RELOAD_MENU_ITEM,
           accelerator: 'CmdOrCtrl+Shift+R',
           click: (item, focusedWindow) => {
             if (focusedWindow) {
@@ -143,29 +249,71 @@ module.exports = (params) => {
         },
         {
           role: 'toggleDevTools',
-          label: i18next.t('menu.viewSubMenu.toggleDevToolsLabel')
+          label: i18next.t('menu.viewSubMenu.toggleDevToolsLabel'),
+          id: MENU_ITEM_IDS.TOGGLE_DEV_TOOLS_MENU_ITEM
         },
         { type: 'separator' },
         {
           role: 'resetZoom',
-          label: i18next.t('menu.viewSubMenu.resetZoomLabel')
+          label: i18next.t('menu.viewSubMenu.resetZoomLabel'),
+          id: MENU_ITEM_IDS.RESET_ZOOM_MENU_ITEM
         },
         {
           role: 'zoomIn',
-          label: i18next.t('menu.viewSubMenu.zoomInLabel')
+          label: i18next.t('menu.viewSubMenu.zoomInLabel'),
+          id: MENU_ITEM_IDS.ZOOM_IN_MENU_ITEM
         },
         {
           role: 'zoomOut',
-          label: i18next.t('menu.viewSubMenu.zoomOutLabel')
+          label: i18next.t('menu.viewSubMenu.zoomOutLabel'),
+          id: MENU_ITEM_IDS.ZOOM_OUT_MENU_ITEM
         },
         { type: 'separator' },
         {
           role: 'togglefullscreen',
-          label: i18next.t('menu.viewSubMenu.togglefullscreenLabel')
+          label: i18next.t('menu.viewSubMenu.togglefullscreenLabel'),
+          id: MENU_ITEM_IDS.TOGGLE_FULL_SCREEN_MENU_ITEM
         }
       ]
     },
-    { role: 'windowMenu', label: i18next.t('menu.windowSubMenu.label') },
+    {
+      role: 'windowMenu',
+      label: i18next.t('menu.windowSubMenu.label'),
+      submenu: [
+        {
+          role: 'minimize',
+          label: 'Minimize', // TODO:
+          id: MENU_ITEM_IDS.MINIMIZE_MENU_ITEM
+        },
+        {
+          role: 'zoom',
+          label: 'Zoom', // TODO:
+          id: MENU_ITEM_IDS.ZOOM_MENU_ITEM
+        },
+        ...(isMac
+          ? [
+              { type: 'separator' },
+              {
+                role: 'front',
+                label: 'Front', // TODO:
+                id: MENU_ITEM_IDS.MAC_FRONT_MENU_ITEM
+              },
+              { type: 'separator' },
+              {
+                role: 'window',
+                label: 'Window', // TODO:
+                id: MENU_ITEM_IDS.MAC_WINDOW_MENU_ITEM
+              }
+            ]
+          : [
+              {
+                role: 'close',
+                label: 'Close', // TODO:
+                id: MENU_ITEM_IDS.CLOSE_MENU_ITEM
+              }
+            ])
+      ]
+    },
     {
       label: i18next.t('menu.toolsSubMenu.label'),
       submenu: [
@@ -174,26 +322,32 @@ module.exports = (params) => {
           submenu: [
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.exportDbLabel'),
+              id: MENU_ITEM_IDS.EXPORT_DB_MENU_ITEM,
               click: exportDB({ pathToUserData, pathToUserDocuments })
             },
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.importDbLabel'),
+              id: MENU_ITEM_IDS.IMPORT_DB_MENU_ITEM,
               click: importDB({ pathToUserData, pathToUserDocuments })
             },
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.restoreDbLabel'),
+              id: MENU_ITEM_IDS.RESTORE_DB_MENU_ITEM,
               click: restoreDB()
             },
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.backupDbLabel'),
+              id: MENU_ITEM_IDS.BACKUP_DB_MENU_ITEM,
               click: backupDB()
             },
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.removeDbLabel'),
+              id: MENU_ITEM_IDS.REMOVE_DB_MENU_ITEM,
               click: removeDB({ pathToUserData })
             },
             {
               label: i18next.t('menu.toolsSubMenu.dataManagementSubMenu.clearAllDataLabel'),
+              id: MENU_ITEM_IDS.CLEAR_ALL_DATA_MENU_ITEM,
               click: removeDB({
                 pathToUserData,
                 shouldAllTablesBeCleared: true
@@ -204,10 +358,12 @@ module.exports = (params) => {
         { type: 'separator' },
         {
           label: i18next.t('menu.toolsSubMenu.changeReportsFolderLabel'),
+          id: MENU_ITEM_IDS.CHANGE_REPORTS_FOLDER_MENU_ITEM,
           click: changeReportsFolder({ pathToUserDocuments })
         },
         {
           label: i18next.t('menu.toolsSubMenu.changeSyncFrequencyLabel'),
+          id: MENU_ITEM_IDS.CHANGE_SYNC_FREQUENCY_MENU_ITEM,
           click: changeSyncFrequency()
         }
       ]
@@ -247,6 +403,7 @@ module.exports = (params) => {
         { type: 'separator' },
         {
           label: i18next.t('menu.helpSubMenu.userManualLabel'),
+          id: MENU_ITEM_IDS.USER_MANUAL_MENU_ITEM,
           accelerator: 'CmdOrCtrl+H',
           click: () => showDocs()
         },
@@ -268,16 +425,13 @@ module.exports = (params) => {
                   'menu.helpSubMenu.aboutLabel',
                   { appName: app.name }
                 ),
+                id: MENU_ITEM_IDS.ABOUT_MENU_ITEM,
                 click: showAboutModalDialog()
               }
             ])
       ]
     }
   ]
-
-  if (params?.shouldMenuTemplateBeReturned) {
-    return menuTemplate
-  }
 
   Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
   isMenuInitialized = true
