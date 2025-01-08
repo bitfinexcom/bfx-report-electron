8e2c7719aa13ba2c4d86e7b73e1452754ac52be2
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-01-08 12:38:41 +0200

    Adjust menu item accelerators for mac

--- src/create-menu/menu.item.accelerators.js
+++ src/create-menu/menu.item.accelerators.js
@@ -3,6 +3,12 @@
 const isMac = process.platform === 'darwin'
 
 const MENU_ITEM_ACCELERATORS = {
+  MAC_HIDE_MENU_ITEM: 'CmdOrCtrl+H',
+  MAC_HIDE_OTHERS_MENU_ITEM: 'CmdOrCtrl+Alt+H',
+  MAC_PASTE_AND_MATCH_STYLE_MENU_ITEM: 'CmdOrCtrll+Alt+Shift+V',
+  MAC_TOGGLE_DEV_TOOLS_MENU_ITEM: 'CmdOrCtrl+Alt+I',
+  MAC_TOGGLE_FULL_SCREEN_MENU_ITEM: 'Option+F',
+
   ABOUT_MENU_ITEM: 'CmdOrCtrl+O',
   QUIT_MENU_ITEM: 'CmdOrCtrl+Q',
   UNDO_MENU_ITEM: 'CmdOrCtrl+Z',
@@ -10,7 +16,6 @@ const MENU_ITEM_ACCELERATORS = {
   CUT_MENU_ITEM: 'CmdOrCtrl+X',
   COPY_MENU_ITEM: 'CmdOrCtrl+C',
   PASTE_MENU_ITEM: 'CmdOrCtrl+V',
-  DELETE_MENU_ITEM: 'Delete',
   SELECT_ALL_MENU_ITEM: 'CmdOrCtrl+A',
   RELOAD_MENU_ITEM: 'CmdOrCtrl+R',
   FORCE_RELOAD_MENU_ITEM: 'CmdOrCtrl+Shift+R',
