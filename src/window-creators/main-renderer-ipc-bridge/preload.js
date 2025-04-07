ba206a9fc4d8a1e73a0c8a8243386ca8975fd1e2
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-04-07 13:29:39 +0300

    Expose close loading window handler via ipc bridge

--- src/window-creators/main-renderer-ipc-bridge/preload.js
+++ src/window-creators/main-renderer-ipc-bridge/preload.js
@@ -13,6 +13,7 @@ const CHANNEL_NAMES = {
 const GENERAL_INVOKE_METHOD_NAMES = {
   EXIT: 'exit',
   MINIMIZE_LOADING_WINDOW: 'minimizeLoadingWindow',
+  CLOSE_LOADING_WINDOW: 'closeLoadingWindow',
   GET_TITLE: 'getTitle'
 }
 const GENERAL_EVENT_METHOD_NAMES = {
