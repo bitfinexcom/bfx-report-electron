a11199cceb4469685564576884bf2c35338d5d27
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-04-23 11:24:37 +0300

    Add ipc method for startup loading window

--- src/window-creators/main-renderer-ipc-bridge/general-ipc-channel-handlers.js
+++ src/window-creators/main-renderer-ipc-bridge/general-ipc-channel-handlers.js
@@ -3,6 +3,7 @@
 const { app } = require('electron')
 
 const wins = require('../windows')
+const WINDOW_NAMES = require('../window.names')
 const IpcChannelHandlers = require('./ipc.channel.handlers')
 
 class GeneralIpcChannelHandlers extends IpcChannelHandlers {
@@ -10,16 +11,16 @@ class GeneralIpcChannelHandlers extends IpcChannelHandlers {
     return app.exit(args?.code ?? 0)
   }
 
+  async getTitleHandler (event, args) {
+    return wins[WINDOW_NAMES.MAIN_WINDOW].getTitle()
+  }
+
   async minimizeLoadingWindowHandler (event, args) {
-    await wins.loadingWindow?.minimize()
+    await wins[WINDOW_NAMES.LOADING_WINDOW]?.minimize()
   }
 
   async closeLoadingWindowHandler (event, args) {
-    await wins.loadingWindow?.close()
-  }
-
-  async getTitleHandler (event, args) {
-    return wins.mainWindow.getTitle()
+    await wins[WINDOW_NAMES.LOADING_WINDOW]?.close()
   }
 
   static onLoadingDescriptionReady (cb) {
@@ -32,6 +33,26 @@ class GeneralIpcChannelHandlers extends IpcChannelHandlers {
 
   static sendLoadingBtnStates (win, args) {
     return this.sendToRenderer(this.sendLoadingBtnStates, win, args)
+  }
+
+  async minimizeStartupLoadingWindowHandler (event, args) {
+    await wins[WINDOW_NAMES.STARTUP_LOADING_WINDOW]?.minimize()
+  }
+
+  async closeStartupLoadingWindowHandler (event, args) {
+    await wins[WINDOW_NAMES.STARTUP_LOADING_WINDOW]?.close()
+  }
+
+  static onStartupLoadingDescriptionReady (cb) {
+    return this.handleListener(this.onStartupLoadingDescriptionReady, cb)
+  }
+
+  static sendStartupLoadingDescription (win, args) {
+    return this.sendToRenderer(this.sendStartupLoadingDescription, win, args)
+  }
+
+  static sendStartupLoadingBtnStates (win, args) {
+    return this.sendToRenderer(this.sendStartupLoadingBtnStates, win, args)
   }
 }
 
