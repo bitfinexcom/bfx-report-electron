4d939afa706eae9027287d9d979645ec2d4258d5
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-10-08 11:39:57 +0300

    Add staging label to db file name for management

--- src/const.js
+++ src/const.js
@@ -1,8 +1,14 @@
 'use strict'
 
+const isBfxApiStaging = require('./helpers/is-bfx-api-staging')
+
+const stagingDbLable = isBfxApiStaging()
+  ? '-staging'
+  : ''
+
 const CONFIGS_FILE_NAME = 'bfx-report-configs.json'
 const DEFAULT_ARCHIVE_DB_FILE_NAME = 'bfx-report-db-archive'
-const DB_FILE_NAME = 'db-sqlite_sync_m0.db'
+const DB_FILE_NAME = `db-sqlite_sync${stagingDbLable}_m0.db`
 const DB_SHM_FILE_NAME = `${DB_FILE_NAME}-shm`
 const DB_WAL_FILE_NAME = `${DB_FILE_NAME}-wal`
 const SECRET_KEY_FILE_NAME = 'secret-key'
