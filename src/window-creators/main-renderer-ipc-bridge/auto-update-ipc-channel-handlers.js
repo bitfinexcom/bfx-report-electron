23d0dbd55d4f6dc10b69747eb91ef6793a3e48a6
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-11-10 14:02:57 +0200

    Add method to send auto-update downloading progress event

--- src/window-creators/main-renderer-ipc-bridge/auto-update-ipc-channel-handlers.js
+++ src/window-creators/main-renderer-ipc-bridge/auto-update-ipc-channel-handlers.js
@@ -8,6 +8,10 @@ class AutoUpdateIpcChannelHandlers extends IpcChannelHandlers {
   static sendFireToastEvent (win, args) {
     return this.sendToRenderer(this.sendFireToastEvent, win, args)
   }
+
+  static sendProgressToastEvent (win, args) {
+    return this.sendToRenderer(this.sendProgressToastEvent, win, args)
+  }
 }
 
 module.exports = AutoUpdateIpcChannelHandlers
