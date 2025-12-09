628bc535a5ca91a15dd5151b22fee5c02ab532d3
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-12-09 13:04:44 +0200

    Register on close modal event method in preload script

--- src/window-creators/main-renderer-ipc-bridge/preload.js
+++ src/window-creators/main-renderer-ipc-bridge/preload.js
@@ -56,6 +56,7 @@ const MODAL_INVOKE_METHOD_NAMES = {
 }
 const MODAL_EVENT_METHOD_NAMES = {
   ON_FIRE_TOAST_EVENT: 'onFireModalEvent',
+  ON_CLOSE_MODAL_EVENT: 'onCloseModalEvent',
   SEND_TOAST_CLOSED_EVENT: 'sendModalClosedEvent'
 }
 
