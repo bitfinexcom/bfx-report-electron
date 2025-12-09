b367a0395937342040986b3fc1ae6f16092e477f
Vladimir Voronkov <vsvoronkov@gmail.com>
2025-12-09 13:04:23 +0200

    Fix set window height method registration

--- src/window-creators/main-renderer-ipc-bridge/preload.js
+++ src/window-creators/main-renderer-ipc-bridge/preload.js
@@ -52,7 +52,7 @@ const AUTO_UPDATE_EVENT_METHOD_NAMES = {
   SEND_TOAST_CLOSED_EVENT: 'sendToastClosedEvent'
 }
 const MODAL_INVOKE_METHOD_NAMES = {
-  ON_FIRE_TOAST_EVENT: 'setWindowHeight'
+  SET_WINDOW_HEIGHT: 'setWindowHeight'
 }
 const MODAL_EVENT_METHOD_NAMES = {
   ON_FIRE_TOAST_EVENT: 'onFireModalEvent',
