f8c073203a86e339237d900fd28d631b1bd51b45
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-07-24 10:31:12 +0300

    Remove redundant dep installation steps for db driver

--- scripts/helpers/install-backend-deps.sh
+++ scripts/helpers/install-backend-deps.sh
@@ -67,16 +67,6 @@ function installBackendDeps {
   npm i --omit=dev --no-audit --progress=false
   checkNodeModulesDir "$WORKER_FOLDER"
   npm ls --depth=0 --omit=dev 1<&-
-  # Starting from npm v11, npm_config_xxx is no longer supported and affects the installation of binaries such as better-sqlite3
-  # https://github.com/npm/cli/issues/8353
-  # https://github.com/npm/cli/issues/8153
-  # https://github.com/WiseLibs/better-sqlite3/blob/master/package.json#L41
-  # https://github.com/WiseLibs/better-sqlite3/blob/master/docs/troubleshooting.md#electron
-  sqliteModuleName="better-sqlite3"
-  npm explore "$sqliteModuleName" -- \
-    npx --yes prebuild-install@7.1.3 --platform="$targetPlatform" --arch="$ARCH" --runtime="electron" --target="$ELECTRON_VER" || \
-    (npx --yes node-gyp@12.2.0 rebuild --release --directory="node_modules/$sqliteModuleName" && \
-    npx --yes @electron/rebuild@4.0.3 --force --which-module="$sqliteModuleName" --version="$ELECTRON_VER" --arch="$ARCH" --dist-url="$DIST_URL")
 
   cd "$INSTALL_BACKEND_CURRDIR"
 }
