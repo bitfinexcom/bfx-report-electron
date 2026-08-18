5fe8129e353072ec295cefd5aaa1213e2fea4830
Vladimir Voronkov <vsvoronkov@gmail.com>
2026-08-18 13:49:59 +0300

    Use new init flow for ipc channel handlers

--- src/initialize-app.js
+++ src/initialize-app.js
@@ -3,26 +3,8 @@
 const { app } = require('electron')
 const i18next = require('i18next')
 
-const TranslationIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/translation-ipc-channel-handlers'
-)
-const GeneralIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/general-ipc-channel-handlers'
-)
-const MenuIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/menu-ipc-channel-handlers'
-)
-const ThemeIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/theme-ipc-channel-handlers'
-)
-const AutoUpdateIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/auto-update-ipc-channel-handlers'
-)
-const ModalIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/modal-ipc-channel-handlers'
-)
-const ReportExportIpcChannelHandlers = require(
-  './window-creators/main-renderer-ipc-bridge/report-export-ipc-channel-handlers'
+const { initIpcChannelHandlers } = require(
+  './window-creators/main-renderer-ipc-bridge/utils'
 )
 const triggerSyncAfterUpdates = require('./trigger-sync-after-updates')
 const triggerElectronLoad = require('./trigger-electron-load')
@@ -43,7 +25,6 @@ const {
 const {
   deserializeError,
   getFreePort,
-  initIpcChannelHandlers,
   manageConfigs,
   platformIdentifiers: {
     IS_WIN
@@ -115,15 +96,9 @@ const _ipcMessToPromise = (ipc) => {
 
 module.exports = async () => {
   try {
-    initIpcChannelHandlers(
-      GeneralIpcChannelHandlers,
-      TranslationIpcChannelHandlers,
-      MenuIpcChannelHandlers,
-      ThemeIpcChannelHandlers,
-      AutoUpdateIpcChannelHandlers,
-      ModalIpcChannelHandlers,
-      ReportExportIpcChannelHandlers
-    )
+    const {
+      ThemeIpcChannelHandlers
+    } = initIpcChannelHandlers()
 
     app.disableHardwareAcceleration()
     app.on('window-all-closed', () => {
